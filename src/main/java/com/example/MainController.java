25a778fa0e53bfe8e87419d5c43273971dd16d08
tsuneyasu <tsuneyasu1209@gmail.com>
2022-09-04 22:16:44 +0900

    second

--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
 	@GetMapping("/")
 	public String write1(Model model) {
 		model.addAttribute("moji", "hello world!");
 		return "hello";
 	}
-
 }
